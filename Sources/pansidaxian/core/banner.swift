0e2cbfd07af16b74fbb0bdc4453a4d6c2fb0eccd
gao8954 <895407214gao@sina.com>
2018-07-19 01:08:08 +0800

    add bundle

--- Sources/pansidaxian/core/banner.swift
+++ Sources/pansidaxian/core/banner.swift
@@ -7,15 +7,26 @@
 
 import Foundation
 
+
+public enum ReaderError: Error {
+    case resourceNotFound
+    case readFailed(Error)
+    case convertToStringFailed
+}
+
 func Banner(){
+    let bundle = Bundle()
+    let banner = "banner"
+    let type = "rtf"
+    let resourcePath = bundle.path(forResource: banner, ofType: type)
     
-    let banner = "./banner.rtf"
+    
     //let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent("banner.rtf")
     
     print(banner)
 
     do{
-        let data = try String(contentsOfFile: banner)
+        let data = try String(contentsOfFile: resourcePath!)
         print(data)
     }catch{
         print(error.localizedDescription)
